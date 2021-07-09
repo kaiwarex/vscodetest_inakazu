d617ecc51f1bd93ca9a046cbacb4e5c02bf8e15b
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-10 05:37:22 +0900

    test19

---
+++ lern_csharp/test19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace test19
+{
+    //基本クラス
+    class Music
+    {
+        public int Type = 0;
+        public string Name = "Music";
+        public void BaseInfo()
+        {
+            Console.WriteLine("Music");
+        }
+    }
+    //サブクラス
+    class Song:Music
+    {
+        public string Key = "C#";
+        public void DrvInfo()
+        {
+            Console.WriteLine("Song");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Song s = new Song();
+
+            Console.WriteLine(s.Key);
+            s.DrvInfo();
+
+            //基本クラスのメンバにアクセスできる
+            Console.WriteLine(s.Type);
+            s.BaseInfo();
+
+            Music m = new Music();
+
+            Console.WriteLine(m.Type);
+            m.BaseInfo();
+        }
+    }
+}
