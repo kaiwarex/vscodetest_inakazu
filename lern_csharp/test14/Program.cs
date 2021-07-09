bfe2e484866aef8cb89aae624ff7d5afd24a0e40
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-09 23:44:53 +0900

    test14です

--- lern_csharp/test14/Program.cs
+++ lern_csharp/test14/Program.cs
@@ -2,11 +2,24 @@
 
 namespace test14
 {
+    class Car
+    {
+        //フィールド
+        public string name = "マイカー";
+        public int seats;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Car test = new Car();
+            Car test2 = new Car();
+
+            test2.name = "サブ";
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(test.name);
+            Console.WriteLine(test2.name);
         }
     }
 }
