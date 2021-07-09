f6e28c67847156dd5fe07d96385b5d04e7467e30
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-10 05:03:41 +0900

    test18

---
+++ lern_csharp/test18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace test18
+{
+    class Car
+    {
+        public string name;
+        public static int seats;
+        public Car()
+        {
+            name = "マンコカー";
+            Console.WriteLine(name);
+        }
+
+        //静的コンストラクタ
+        static Car()
+        {
+            seats = 4;
+            Console.WriteLine(seats);
+        }
+
+        //静的メソッド
+        public static void SetSeat(int n)
+        {
+            seats = n; //静的メンバに代入           
+        }
+
+        public void Display(){
+            Console.WriteLine(seats);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car test = new Car();
+            Car.SetSeat(5);
+            Car test2 = new Car();
+            test2.Display();
+        }
+    }
+}
