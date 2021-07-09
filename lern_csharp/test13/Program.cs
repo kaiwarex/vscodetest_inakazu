b2c187b4a41a44e012fee410b324986b0df0b24e
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-09 20:49:49 +0900

    main

---
+++ lern_csharp/test13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace test13
+{
+    class Car
+    {
+        //データメンバ
+        public string name;
+        public int seats;
+        //コンストラクタ
+        public Car()
+        {
+            seats = 4;
+        }
+        //string型のパラメーターを受け取るコンストラクタ
+        public Car(string str)
+        {
+            seats = 4;
+            name = str;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car test = new Car();
+            Console.WriteLine(test.name);
+            Car test2 = new Car("マイカー");
+            Console.WriteLine(test2.name);
+        }
+    }
+}
