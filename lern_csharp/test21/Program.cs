c22badc21d6d95ab13bef10c65de39b5cf6f52f4
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-17 12:06:00 +0900

    test22

---
+++ lern_csharp/test21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace test21
+{
+    //基本クラス
+    class Music
+    {
+        public Music()
+        {
+            Console.WriteLine("基本クラスのコンストラクタ");
+        }
+    }
+    //派生クラス
+    class Song:Music
+    {
+        public string Key = "C#";
+        public Song()
+        {
+            Console.WriteLine("派生クラスのコンストラクタ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song s = new Song();
+        }
+    }
+}
---
+++ lern_csharp/test22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace test22
+{
+    //基本クラス
+    class BaseClass
+    {
+        //コンストラクタ
+        public BaseClass (string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    //派生クラス
+    class DerivedClass:BaseClass
+    {
+        public DerivedClass(string s1,string s2):base(s2)
+        {
+            Console.WriteLine(s1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var c = new DerivedClass("Derived","Base");
+        }
+    }
+}
