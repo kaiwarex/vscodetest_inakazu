8319e79935629838ffe7a888726ed5054974eb06
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-23 19:30:22 +0900

    どうかな

---
+++ lern_csharp/test26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace test26
+{
+    //基本クラス
+    class Music
+    {
+        public virtual void BaseInfo()
+        {
+            Console.WriteLine("Music");
+        }
+    }
+    //派生クラス
+    class Music2:Music
+    {
+        public override void BaseInfo()
+        {
+            Console.WriteLine("Song");
+        }
+    }
+    //派生クラス
+    // class Music２:Music
+    // {
+    //     new public void BaseInfo()
+    //     {
+    //         Console.WriteLine("Music2");
+    //     }
+    // }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Song s = new Song();
+            s.BaseInfo();
+
+            Music2 m2 = new Music2();
+            m2.BaseInfo();
+
+            Music m = new Music();
+            m.BaseInfo();
+        }
+    }
+}
---
+++ lern_csharp/test27/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace test27
+{
+    //基本クラス
+    class Music
+    {
+        public virtual void BaseInfo()
+        {
+            Console.WriteLine("Music");
+        }
+    }
+    //派生クラス
+    class Song:Music
+    {
+        public override void BaseInfo()
+        {
+            Console.WriteLine("Song");
+        }
+    }
+    //派生クラス
+    class Music2:Music
+    {
+        new public void BaseInfo()
+        {
+            Console.WriteLine("Music2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Song s = new Song();
+            s.BaseInfo();
+
+            Music2 m2 = new Music2();
+            m2.BaseInfo();
+
+            Music m = new Music();
+            m.BaseInfo();
+        }
+    }
+}
