f78f812e5abfcf501fa1684cdff5176aa864c580
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-24 11:11:28 +0900

    test29

---
+++ lern_csharp/test28/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace test28
+{
+    //基本クラス
+    class Music
+    {
+        int type = 0;
+        protected string name = "Music";
+        // typeフィールドに値を設定するpublicメソッド
+        public int Type
+        {
+            set
+            {
+                this.type = value;
+            }
+            get
+            {
+                return this.type;
+            }
+        }
+        //フィールドの値を出力する仮想メソッド
+        public virtual void PrintName()
+        {
+            Console.WriteLine(type);
+            Console.WriteLine(name);
+        }
+    }
+    //派生クラス
+    class Song:Music
+    {
+        public override void PrintName()
+        {
+            //this.type = 1;
+            this.name = "Song";
+            base.PrintName();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Song s = new Song();
+            s.Type = 1;
+            // s.name = "test";
+            s.PrintName();
+        }
+    }
+}
---
+++ lern_csharp/test29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test29
+{
+    class SampleClass
+    {
+        public string Type {get;set;} = "nothing";
+        public string Name {get;} = "abc";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new SampleClass();
+            Console.WriteLine(test.Type);   //nothing
+            Console.WriteLine(test.Name);   //abc
+        }
+    }
+}
