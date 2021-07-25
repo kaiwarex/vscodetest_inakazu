824a8fcf766391251be54c8e351aab788e27f65d
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-25 20:43:44 +0900

    2021/07/25　20:43　変更分

--- lern_csharp/test34/Program.cs
+++ lern_csharp/test34/Program.cs
@@ -2,11 +2,27 @@
 
 namespace test34
 {
+    class SampleClass
+    {
+        //フィールド
+        public int v1 = 10;
+        private int v2;
+
+        //デフォのコンストラクタ
+        public SampleClass()
+        {
+            Console.WriteLine(this.v1);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            var test = new SampleClass{v1 = 20};
+            Console.WriteLine(test.v1);
+
+            //privateのメンバーは設定出来ないのでコンパイルエラーとなる
+            //var test2 = new SampleClass{v2 = 20}
     }
 }
