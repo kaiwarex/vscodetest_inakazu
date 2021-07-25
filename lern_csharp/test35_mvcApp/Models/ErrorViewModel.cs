711d3469578a349a116e48dfa5d0a80b270c1e7d
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-25 21:40:48 +0900

    2021/07/25　21:40

---
+++ lern_csharp/test35_mvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test35_mvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
