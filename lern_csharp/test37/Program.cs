3fde3ddd69146f0504d357ac16db9c02c2f6b92f
kaiwarex <pain.of.rebirth@gmail.com>
2021-07-29 04:20:38 +0900

    2021/07/29

---
+++ lern_csharp/test37/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test37
+{
+    class TestArray
+    {
+        //privateなので外部からはアクセス出来ない
+        int[] array = new int[3];
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
